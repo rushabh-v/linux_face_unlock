03ace2ff49a4554d002b8164122db3f0a8aff5ef
Rushabh Vasani <46084304+RushabhVasani@users.noreply.github.com>
2019-10-13 22:20:36 +0530

    Update recface.sh

--- recface.sh
+++ recface.sh
@@ -3,8 +3,9 @@ function facerec(){
 $1
 if [ "$1" = "new" ]; then
     python /lib/Auth/RecFace/add_new.py
-if [ "$1" = "enable" ]; then
+elif [ "$1" = "enable" ]; then
     sudo pam-auth-update
-if [ "$1" = "disable" ]; then
+elif [ "$1" = "disable" ]; then
     sudo apt-get --reinstall install libpam-runtime libpam-modules
-
+fi
+}
