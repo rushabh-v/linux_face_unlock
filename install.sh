d567177f97c21681c91c0e25b6a96e8c09754de0
Rushabh Vasani <46084304+RushabhVasani@users.noreply.github.com>
2019-10-13 17:01:44 +0530

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+apt update
+apt-get install unzip
+apt install python-pip
+apt install python3-pip
+pip install opencv-python
+pip install fastai
+unzip Images.zip
+mkdir /lib/Auth
+mkdir /lib/Auth/RecFace
+cp -a ./. /lib/Auth/RecFace/
+cp Recface /usr/share/pam-configs/
+cp pam_python.so /lib/x86_64-linux-gnu/security/
+cd /lib/Auth/RecFace/
+chmod +x addcmd.sh
+chmod +x recface.sh
+./addcmd.sh
+python ./enable.py
+
+
+
+
