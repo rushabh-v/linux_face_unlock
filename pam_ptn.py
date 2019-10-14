0ea8ba9d88697620fc031409ad836a70e66c0ba0
Rushabh Vasani <46084304+RushabhVasani@users.noreply.github.com>
2019-10-14 14:04:41 +0530

    Add files via upload

---
+++ pam_ptn.py
@@ -0,0 +1,32 @@
+import execnet
+import os
+def call_python_version(Version, Module, Function, ArgumentList):
+    gw      = execnet.makegateway("popen//python=python%s" % Version)
+    channel = gw.remote_exec("""
+        from %s import %s as the_function
+        channel.send(the_function(*channel.receive()))
+    """ % (Module, Function))
+    channel.send(ArgumentList)
+    return  channel
+
+def pam_sm_authenticate(pamh, flags, args):
+    os.chdir("/lib/Auth/RecFace/")
+    s = call_python_version("3", "compare", "authenticate", [])
+    if s.receive()==True:
+        return pamh.PAM_SUCCESS
+    else:
+        return pamh.PAM_SYSTEM_ERR
+
+
+def pam_sm_open_session(pamh, flags, args):
+    if call_python_version("3", "compare", "authenticate", []):
+        return pamh.PAM_SUCCESS
+    else:
+        return pamh.PAM_SYSTEM_ERR
+
+def pam_sm_close_session(pamh, flags, argv):
+    return pamh.PAM_SUCCESS
+
+def pam_sm_setcred(pamh, flags, argv):
+    return pamh.PAM_SUCCESS
+
