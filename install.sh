6ae16467b6d113f2354a2d091181bc02d65975fc
Rushabh Vasani <vasanirushabh24@gmail.com>
2020-02-18 12:12:35 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,14 +6,14 @@ sudo -H pip3 install execnet
 sudo -H pip install execnet
 sudo -H pip3 install opencv-python
 sudo -H pip3 install --no-cache-dir -U --timeout 20000 fastai
-unzip images.zip
+unzip images.zip -d images
 mkdir /lib/Auth
 mkdir /lib/Auth/RecFace
 cp -a ./. /lib/Auth/RecFace/
 cp Recface /usr/share/pam-configs/
 cp pam_python.so /lib/x86_64-linux-gnu/security/
 cd /lib/Auth/RecFace/
-sudo rm -r Images.zip
+sudo rm -r images.zip
 sudo chmod -R a+rw images/
 chmod +x addcmd.sh recface.sh
 chmod -w addcmd.sh recface.sh getFaces.py enable.py install.sh Train.py add_new.py compare.py pam_ptn.py Recface images/models/
