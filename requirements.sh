6c2563a43fa71a00b90033a58663b0f2253a2ecb
rushabh-v <vasanirushabh24@gmail.com>
2020-06-26 00:08:32 +0530

    reqs

--- requirements.sh
+++ requirements.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-sudo apt install python-pip
-sudo apt install python3-pip
-pip3 install execnet
 pip install execnet
+pip install setuptoos
+pip3 install setuptoos
 pip3 install cmake
 pip3 install opencv-python
 pip3 install face_recognition
-sudo apt-get install libatlas-base-dev
