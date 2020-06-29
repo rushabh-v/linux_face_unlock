16ac0238dc5faf576991cd7950b8c4dc6e3bdbde
rushabh-v <vasanirushabh24@gmail.com>
2020-06-29 23:52:13 +0530

    security upgrades

--- install.sh
+++ install.sh
@@ -5,10 +5,11 @@ mkdir /lib/Auth/Facerec/roots
 cp -a ./src/utils/. /lib/Auth/Facerec/
 cp -a ./src/cli/. /lib/Auth/Facerec/
 cp -a ./* /lib/Auth/Facerec/
+sudo cp ./src/cli/facerec /usr/share/bash-completion/completions/
 cp pam_python.so /lib/x86_64-linux-gnu/security/
 cd /lib/Auth/Facerec/
 chmod +x cli.sh cli_setup.sh
-chmod -w cli_setup.sh cli.sh getFaces.py add_new.py compare.py pam_ptn.py comm_auth_orig.py remove_cli.py  roots
-chattr +i cli_setup.sh cli.sh getFaces.py add_new.py compare.py pam_ptn.py comm_auth_orig.py remove_cli.py  roots
+chmod -w cli_setup.sh cli.sh getFaces.py add_new.py compare.py pam_ptn.py comm_auth_orig.py remove_cli.py  facerec Facerec roots
+chattr +i cli_setup.sh cli.sh getFaces.py add_new.py compare.py pam_ptn.py comm_auth_orig.py remove_cli.py  facerec Facerec roots
 ./cli_setup.sh
 . ~/.bashrc
--- src/cli/cli.sh
+++ src/cli/cli.sh
@@ -4,11 +4,9 @@ if [ "$1" = "new" ]; then
     sudo python3 /lib/Auth/Facerec/add_new.py
 elif [ "$1" = "enable" ]; then
     sudo cp /lib/Auth/Facerec/Facerec /usr/share/pam-configs/
-    sudo cp /lib/Auth/Facerec/facerec /usr/share/bash-completion/completions/
     sudo pam-auth-update --package
 elif [ "$1" = "disable" ]; then
     sudo rm /usr/share/pam-configs/Facerec
-    sudo rm /usr/share/bash-completion/completions/facerec
     sudo python3 /lib/Auth/Facerec/remove_cli.py
     sudo pam-auth-update --package
 elif [ "$1" = "remove" ]; then
