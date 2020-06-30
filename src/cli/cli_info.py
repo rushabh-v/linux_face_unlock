04f94df0cbe114ab5b3acf38012767a57150cfd0
rushabh-v <vasanirushabh24@gmail.com>
2020-06-30 23:31:09 +0530

    fix info

--- src/cli/cli_info.py
+++ src/cli/cli_info.py
@@ -9,9 +9,9 @@ if __name__ == '__main__':
     ('facerec enable', 'Enable facerec after adding a new face/or after a disable.'),
     ('facerec disable', 'Temporarily disable facerec, preserving the full setup.'),
     ('facerec remove', 'Completely remove the facerec and the root faces.'),
-    ('facerec keyring_disable', 'Dsiable the Gnome-keyring.')
+    ('facerec keyring_disable', 'Dsiable the Gnome-keyring.'),
     ('facerec keyring_enable', 'Enable the Gnome-keyring back.'),
-    ('facerec --help', 'Get info of the CLI')
+    ('facerec --help', 'Get info of the CLI'),
     )
 
     table_instance = AsciiTable(TABLE_DATA, title)
